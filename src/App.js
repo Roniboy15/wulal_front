1eb87d9d07acdcb689db90ac7bfc563c2a204aef
Your Name <you@example.com>
2023-10-17 15:08:24 +0300

    quotes-major-changes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import React, { useEffect, useRef, useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import FlipBook from './components/FlipBook/FlipBook';
 import NavBar from './components/NavBar/NavBar';
 import './App.css'
 import { doApiGet } from './services/springbootAPI';
 import Loader from './general_comps/loader/loader';
 import useWindowWidth from './general_comps/useWidth';
+import Quotes from './components/Quotes/Quotes';
+import LanguageSelection from './components/LanguageSelection/Language';
 
 function App() {
 
-    const [currentPage, setCurrentPage] = useState(localStorage.getItem('savedCurrentPage')?parseInt(localStorage.getItem('savedCurrentPage'), 10):0);
+    const [currentPage, setCurrentPage] = useState(localStorage.getItem('savedCurrentPage') ? parseInt(localStorage.getItem('savedCurrentPage'), 10) : 0);
     const [language, setLanguage] = useState();
-    const [data, setData] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [showLanguageSelection, setShowLanguageSelection] = useState(true); // New state for language selection visibility
+    const [prayers, setPrayers] = useState([]);
+    const [quotes, setQuotes] = useState([]);
+    const [loadingPrayers, setLoadingPrayers] = useState(true);
+    const [loadingQuotes, setLoadingQuotes] = useState(true);
+    const [showMenu, setShowMenu] = useState(true); // New state for language selection visibility
     const [dropDownActive, setDropDownActive] = useState(false);
+
+    const [showLangBtn, setShowLangBtn] = useState(false);
+
     const width = useWindowWidth();
+    const timeoutRef = useRef(null);
+
 
     const fetchJSONS = async () => {
         let url = '/file/fetch?folder=' + language;
@@ -22,15 +32,27 @@ function App() {
             const response = await doApiGet(url);
             response.sort((a, b) => a.id - b.id);
 
-            setData(response);
-            setLoading(false)
+            setPrayers(response);
+            setLoadingPrayers(false)
 
         } catch (err) {
             console.log(err);
         }
     };
 
-    const timeoutRef = useRef(null);
+    const fetchQuotes = async () => {
+        let url = '/file/download/quotes.json';
+        try {
+            const response = await doApiGet(url);
+            // response.sort((a, b) => a.id - b.id);
+            setQuotes(response);
+            setLoadingQuotes(false)
+            setShowMenu(!showMenu)
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
 
     useEffect(() => {
         localStorage.setItem('savedCurrentPage', currentPage);
@@ -38,12 +60,12 @@ function App() {
         if (timeoutRef.current) {
             clearTimeout(timeoutRef.current);
         }
-    
+
         // Set a new timeout to clear the saved page after one hour
         timeoutRef.current = setTimeout(() => {
             localStorage.removeItem('savedCurrentPage');
         }, 3600000); // 3600000ms is one hour
-    
+
         // When the component is unmounted or before setting a new timeout, clear the existing one
         return () => {
             if (timeoutRef.current) {
@@ -58,48 +80,83 @@ function App() {
         }
     }, [language])
 
+
+
+
+
     const handleLanguageSelection = (selectedLanguage) => {
-        setLanguage(selectedLanguage);
-        setShowLanguageSelection(false); // Hide language selection screen
+        if (selectedLanguage != language) {
+            setLoadingPrayers(true)
+            setLanguage(selectedLanguage); //this will trigger the useEffect which then calls the fetchJSONS()
+        }
     };
 
     useEffect(() => {
         let timeoutId;
-        if (!showLanguageSelection) {
-            timeoutId = setTimeout(() => {
-                if (loading) alert("Try to reload or no connection to the server")
-            }, 10000)
-        }
+        timeoutId = setTimeout(() => {
+            if (loadingPrayers && loadingQuotes) alert("Try to reload or no connection to the server")
+            
+        }, 10000)
+
         return () => clearTimeout(timeoutId); // Clear timeout if the component is unmounted or if the effect re-runs
-    }, [showLanguageSelection, loading])
+    }, [loadingPrayers, loadingQuotes])
 
     return (
-        <>
-            {showLanguageSelection ?
-                <div className="language-selection">
-                    <div className='row'>
-                        <div className='col-12 text-center'>
-                            <img width={width < 500 ? 250 : 350} src='https://res.cloudinary.com/dg4sxlbfs/image/upload/v1693400461/wulal/091596CA-1BB3-4C99-9A78-25308D9CDA3D_xiqj8i.jpg'></img>
-                        </div>
-                        <div className='col-12 text-center mt-3'>
-                            <button className=' btn-lang' onClick={() => handleLanguageSelection('deutsch')}>Deutsch</button>
-                            <button className=' btn-lang' onClick={() => handleLanguageSelection('english')}>English</button>
-                        </div>
-                    </div>
-                </div>
-                : (!loading ?
-                    <div className="App">
-                        <NavBar pages={data} lang={language} onPageSelect={setCurrentPage} setDropDownActive={setDropDownActive} dropDownActive={dropDownActive} />
-                        <div className='flipBook'>
-                            <FlipBook className='mt-5' pages={data} currentPage={currentPage} setCurrentPage={setCurrentPage} dropDownActive={dropDownActive} />
+        <BrowserRouter>
+            <div className="App">
 
-                        </div>
-                    </div>
-                    : <Loader />)
-            }
-        </>
+                <Routes>
+                    {/* Route for Language Selection */}
+                    <Route path="/" element={
+                        <LanguageSelection
+                            setShowLangBtn={setShowLangBtn}
+                            handleLanguageSelection={handleLanguageSelection}
+                            width={width}
+                            fetchQuotes={fetchQuotes}
+                            showLangBtn={showLangBtn}
+                        />}
+                    />
+
+                    {/* Route for FlipBook */}
+                    <Route path="/flipbook" element={loadingPrayers ? <Loader /> :
+                        <>
+                            <NavBar
+                                pages={prayers}
+                                lang={language}
+                                onPageSelect={setCurrentPage}
+                                setDropDownActive={setDropDownActive}
+                                dropDownActive={dropDownActive}
+
+                            />
+                            <FlipBook
+                                className='mt-5'
+                                pages={prayers}
+                                currentPage={currentPage}
+                                setCurrentPage={setCurrentPage}
+                                dropDownActive={dropDownActive}
+                            />
+                        </>
+                    } />
+
+
+                    {/* Route for Quotes */}
+                    <Route path="/quotes" element={loadingQuotes ? <Loader /> :
+                        <>
+                            <NavBar
+                                lang={language}
+                                setDropDownActive={setDropDownActive}
+                                dropDownActive={dropDownActive}
+
+                            />
+                            <Quotes quotesData={quotes} />
+                        </>
+                    } />
+                </Routes>
+            </div>
+        </BrowserRouter>
     );
-
 }
+
+
 
 export default App;
---
+++ src/components/LanguageSelection/Language.jsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+
+function LanguageSelection({ setShowLangBtn, handleLanguageSelection, width, fetchQuotes, showLangBtn }) {
+    return (
+        <div className="language-selection">
+            <div className='row'>
+                <div onClick={()=>setShowLangBtn(!showLangBtn)} className='col-12 text-center'>
+                    <img width={width < 500 ? 250 : 350} src='https://res.cloudinary.com/dg4sxlbfs/image/upload/v1693400461/wulal/091596CA-1BB3-4C99-9A78-25308D9CDA3D_xiqj8i.jpg'></img>
+                </div>
+                <div className='col-12 text-center mt-3'>
+                    {showLangBtn ?
+                        <div className="mt-4">
+                            <Link style={{textDecoration:"none"}} to={"/flipbook"} className=' btn-lang p-2' onClick={() => handleLanguageSelection('deutsch')}>Deutsch</Link>
+                            <Link style={{textDecoration:"none"}} to={"/flipbook"} className=' btn-lang p-2' onClick={() => handleLanguageSelection('english')}>English</Link>
+                        </div>
+                        :
+                        <div className="">
+                            <Link style={{textDecoration:"none"}} to={"/quotes"} className='btn-lang p-2' onClick={() => fetchQuotes()}>Quotes</Link>
+                            <button className='btn-lang border-0 p-2' onClick={() => setShowLangBtn(true)}>Prayer Sheets</button>
+                        </div>}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default LanguageSelection;
