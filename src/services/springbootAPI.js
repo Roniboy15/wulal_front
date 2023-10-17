bfa1c3908e05d08f7b01141bda42ba73e23ae5b2
Your Name <you@example.com>
2023-10-17 15:17:48 +0300

    ok

--- src/services/springbootAPI.js
+++ src/services/springbootAPI.js
@@ -2,28 +2,6 @@ import axios from 'axios';
 
 const API_KEY = 'https://wulal-886ecc4c7ff3.herokuapp.com';
 
-// // Function to get the token from local storage
-// const getToken = () => {
-//     return localStorage.getItem(TOKEN_KEY) || '';
-//   };
-  
-//   export const doApiMethod = async (url, method, data = {}) => {
-//     try {
-//       let res = await axios({
-//         method,
-//         url: API_KEY + url,
-//         data,
-//         headers: {
-//           'Content-Type': 'application/json',
-//           'authorization': `${getToken()}`
-//         },
-//       });
-//       return res.data;
-//     } catch (err) {
-//       console.log(err);
-//       throw err;
-//     }
-//   };
   
   export const doApiGet = async (url) => {
     try {
