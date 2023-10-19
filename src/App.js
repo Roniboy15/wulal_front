88fc31ca0d1bba3a3cbb49a80b50fe6d6785698e
Your Name <you@example.com>
2023-10-19 16:16:29 +0300

    ok

--- src/App.js
+++ src/App.js
@@ -3,62 +3,16 @@ import { BrowserRouter } from 'react-router-dom'
 import AppContent from './AppContent'
 
 const App = () => {
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import AppContent from './AppContent'
+
+const App = () => {
     return (
         <BrowserRouter>
-            <div className="App">
-
-                <Routes>
-                    {/* Route for Language Selection */}
-                    <Route path="/" element={
-                        <LanguageSelection
-                            setShowLangBtn={setShowLangBtn}
-                            handleLanguageSelection={handleLanguageSelection}
-                            width={width}
-                            fetchQuotes={fetchQuotes}
-                            showLangBtn={showLangBtn}
-                        />}
-                    />
-
-                    {/* Route for FlipBook */}
-                    <Route path="/flipbook" element={loadingPrayers ? <Loader /> :
-                        <>
-                            <NavBar
-                                pages={prayers}
-                                lang={language}
-                                onPageSelect={setCurrentPage}
-                                setDropDownActive={setDropDownActive}
-                                dropDownActive={dropDownActive}
-
-                            />
-                            <FlipBook
-                                className='mt-5'
-                                pages={prayers}
-                                currentPage={currentPage}
-                                setCurrentPage={setCurrentPage}
-                                dropDownActive={dropDownActive}
-                            />
-                        </>
-                    } />
-
-
-                    {/* Route for Quotes */}
-                    <Route path="/quotes" element={loadingQuotes ? <Loader /> :
-                        <>
-                            <NavBar
-                                lang={language}
-                                setDropDownActive={setDropDownActive}
-                                dropDownActive={dropDownActive}
-
-                            />
-                            <Quotes quotesData={quotes} />
-                        </>
-                    } />
-                </Routes>
-            </div>
+            <AppContent />
         </BrowserRouter>
-    );
+    )
 }
-
-
 
 export default App;
