8554fdba8c999124e164ae620a3585575a2640ad
Your Name <you@example.com>
2023-10-19 16:16:29 +0300

    ok

--- src/components/LanguageSelection/Language.jsx
+++ src/components/LanguageSelection/Language.jsx
@@ -4,7 +4,7 @@ function LanguageSelection({ setShowLangBtn, handleLanguageSelection, width, fet
     return (
         <div className="language-selection">
             <div className='row'>
-                <div onClick={()=>setShowLangBtn(!showLangBtn)} className='col-12 text-center'>
+                <div onClick={()=>setShowLangBtn(false)} className='col-12 text-center'>
                     <img width={width < 500 ? 250 : 350} src='https://res.cloudinary.com/dg4sxlbfs/image/upload/v1693400461/wulal/091596CA-1BB3-4C99-9A78-25308D9CDA3D_xiqj8i.jpg'></img>
                 </div>
                 <div className='col-12 text-center mt-3'>
